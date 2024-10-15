80fc40a15d0654872ecc2fb980c838502167d535
Md Emtazul Haque <shifat63csedu@gmail.com>
2024-10-15 13:50:40 +0200

    request to DDP in localhost

--- src/main/java/de/medizininformatik_initiative/process/report/service/CreateJson.java
+++ src/main/java/de/medizininformatik_initiative/process/report/service/CreateJson.java
@@ -33,7 +33,7 @@ public class CreateJson extends AbstractServiceDelegate
 		logger.info("Inside the CreateJSON class");
 
 		// URL from the curl command
-		String url = "http://ukb3354.klink.bn:9091/createJson";
+		String url = "localhost:9091/createJson";
 
 		// Credentials from the curl command
 		String username = "ukb";
